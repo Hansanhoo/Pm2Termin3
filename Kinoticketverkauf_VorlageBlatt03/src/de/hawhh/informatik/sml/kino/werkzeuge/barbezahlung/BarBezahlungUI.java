5d90ec586793db468068eb890f73a2c412868f35
DESKTOP-RAROMPM\hansanhoo <hansanho91@gmail.com>
2018-05-29 23:37:39 +0200

    -first

---
+++ Kinoticketverkauf_VorlageBlatt03/src/de/hawhh/informatik/sml/kino/werkzeuge/barbezahlung/BarBezahlungUI.java
@@ -0,0 +1,111 @@
+package de.hawhh.informatik.sml.kino.werkzeuge.barbezahlung;
+
+
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class BarBezahlungUI
+{
+
+	Button _bestaetigenButton;
+	Button _beendenButton;
+	
+	TextField _txtGeldBekommen;
+	Label _lblZuBezahlenderPreis;
+	Label _txtRueckgeld;
+	Dialog<Boolean> _dialog;
+  	public BarBezahlungUI() {
+  		 _dialog = getDialog();
+  	}
+  	/**
+  	 * creates a new Dialog with TextFields Labels and Buttons
+  	 * @return new Dialog
+  	 */
+  	public Dialog<Boolean> getDialog(){
+  		//Create Button
+  		_bestaetigenButton = new Button("Ok");
+  		_beendenButton = new Button("Abbrechen"); 	
+  		
+  		Dialog<Boolean> dialog = new Dialog<>();  		
+  		
+  		// Create the Grid
+  		GridPane grid = new GridPane();
+  		grid.setHgap(10);
+  		grid.setVgap(10);
+  		grid.setPadding(new Insets(20, 150, 10, 10));
+
+  		//Create TextFields&Labels
+  		_lblZuBezahlenderPreis = new Label();
+  		_txtGeldBekommen = new TextField();  		
+  		_txtRueckgeld = new Label();
+
+  		//Add TextFields and Labels to Grid
+  		grid.add(new Label("Zu Bezahlender Betrag:"), 0, 0);
+  		grid.add(_lblZuBezahlenderPreis, 1, 0);
+  		grid.add(new Label("Bekommen:"), 0, 1);
+  		grid.add(_txtGeldBekommen, 1, 1);
+  		grid.add(new Label("Rückgeld:"), 0, 2);
+  		grid.add(_txtRueckgeld, 1, 2);
+  		grid.add(_bestaetigenButton, 1,4);
+  		grid.add(_beendenButton, 2,4);
+  		
+  		//Modaler Dialog
+  		dialog.initModality(Modality.APPLICATION_MODAL); 
+  		dialog.getDialogPane().setContent(grid);  		
+  		
+  		return dialog;  		
+  	}
+  	/**
+  	 * 
+  	 * @return bestaetigenButton
+  	 */
+  	public Button getBestaetigenButton() {
+  		return _bestaetigenButton;
+  	}
+  	/**
+  	 * 
+  	 * @return beendenButton
+  	 */
+  	public Button getBeendenButton() {
+  		return _beendenButton;
+  	}
+  	/**
+  	 * 
+  	 * @return txtGeldBekommen
+  	 */
+  	public TextField getTxtGeldBekommen() {
+  		return _txtGeldBekommen;
+  	}
+  	/**
+  	 * 
+  	 * @return txtRueckgeld
+  	 */
+  	public Label getTxtRueckgeld() {
+  		return _txtRueckgeld;
+  	}
+  	/**
+  	 * 
+  	 * @return txtZuBezahlenderPreis
+  	 */
+  	public Label getLblZuBezahlenderPreis() {
+  		return _lblZuBezahlenderPreis;
+  	}
+  	/**
+  	 * 
+  	 * @return this dialog(UI)
+  	 */
+  	public Dialog<Boolean> getUIPane(){
+  		return _dialog;
+  	}
+	
+}
+	
---
+++ Kinoticketverkauf_VorlageBlatt03/src/de/hawhh/informatik/sml/kino/werkzeuge/barbezahlung/BarBezahlungsWekzeug.java
@@ -0,0 +1,101 @@
+package de.hawhh.informatik.sml.kino.werkzeuge.barbezahlung;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+public class BarBezahlungsWekzeug
+{	
+	private int _zuBezahlenderPreis;
+	private int _bekommendesGeld;
+	private int _rueckGeld;
+	private BarBezahlungUI _ui;
+	
+	
+	/**
+	 * Konstruktor zur Barbezahlung
+	 * @param preisFuerAuswahl
+	 */
+	public BarBezahlungsWekzeug(int preisFuerAuswahl) {
+		_zuBezahlenderPreis = preisFuerAuswahl;		
+	}
+	/**
+	 * startet die UI
+	 * gibt True zurück, falls Ok gedrückt wurde
+	 * False falls Abbrechen
+	 * @return boolean result
+	 */
+	public boolean startBarbezahlung() {
+		_ui = new BarBezahlungUI();
+		_ui.getBestaetigenButton().setDisable(true);
+		setZuBezahlendenrPreis();		
+		registriereUIAktionen();
+		_ui.getUIPane().showAndWait();
+
+		return _ui.getUIPane().resultProperty().get();
+	}
+
+	/**
+	 * Setzt den zu bezahlnden Preis auf TextField txtZuBezahlenderPreis
+	 */
+	private void setZuBezahlendenrPreis() {		
+		_ui.getLblZuBezahlenderPreis().setText(String.valueOf(_zuBezahlenderPreis));				
+	}
+	 /**
+     * Fügt der UI die Funktionalität hinzu mit entsprechenden Listenern.
+     */
+    private void registriereUIAktionen()
+    {
+    	//Alert mit Rückgeld und result true
+    	_ui.getBestaetigenButton().setOnAction(e -> {
+    		Alert alert = new Alert(AlertType.CONFIRMATION,"Tickets verkauft! Bitte geben Sie " + centZuEuro(_rueckGeld) +" zurück!", ButtonType.OK);
+    		
+    		_ui.getUIPane().setResult(true);    		
+    		alert.show();
+    		closeDialog();    
+    	});
+    	//result false und dialog schließen
+     	_ui.getBeendenButton().setOnAction(e -> {
+    		_ui.getUIPane().setResult(false);    		
+    		closeDialog();    
+    	});
+     	
+     	//Bei Eingabe wird das Rückgeld errechnet 
+    	_ui.getTxtGeldBekommen().setOnKeyReleased( e -> {
+    		_rueckGeld = 0;
+    		_ui.getBestaetigenButton().setDisable(true);
+    		
+    		if(_ui.getTxtGeldBekommen().getText().length() > 0) {
+    			_bekommendesGeld = Integer.valueOf(_ui.getTxtGeldBekommen().getText());    	
+    			_rueckGeld = _bekommendesGeld -_zuBezahlenderPreis;    			
+    		 	if((_bekommendesGeld -_zuBezahlenderPreis) >=0){
+    		 		_ui.getBestaetigenButton().setDisable(false);
+    		 	}
+    		
+    			_ui.getTxtRueckgeld().setText(String.valueOf(_rueckGeld));
+    		}
+    		else {		
+    			_ui.getTxtRueckgeld().setText("0");
+    		}
+    	});     
+    }
+    /**
+     * Close the Dialog
+     */
+    private void closeDialog() {
+    	_ui.getUIPane().close();
+    }
+   /**
+    * Rechnet Cent zu Euro
+    * @param cent
+    * @return Euro als String
+    */
+    private String centZuEuro(int cent) {
+    	double betrag = Double.valueOf(cent);    	
+    	betrag /= 100.00;  
+    	
+    	return betrag + " Euro ";
+    	
+    }
+}
